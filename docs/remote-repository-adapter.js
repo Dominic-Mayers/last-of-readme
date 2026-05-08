2f044f0d0517dcc9981b4f2ae2de39c0281ad980
Dominic Mayers <dominic.mayers@meditationstudies.org>
2026-05-08 00:38:30 -0400

    Remove legacyGitHubRemoteToUrls

--- docs/remote-repository-adapter.js
+++ docs/remote-repository-adapter.js
@@ -3,18 +3,14 @@ function createRemoteRepositoryAPI(remote, urlPath = '') {
     throw new Error('A GitHub remote is required');
   }
 
-  const resolvedRemote = remote.repositoryApiUrl && remote.repositoryBrowserUrl
-    ? remote
-    : legacyGitHubRemoteToUrls(remote);
-
-  if (!resolvedRemote.repositoryApiUrl || !resolvedRemote.repositoryBrowserUrl) {
+  if (!remote.repositoryApiUrl || !remote.repositoryBrowserUrl) {
     throw new Error(
       'A GitHub remote with repositoryApiUrl and repositoryBrowserUrl is required'
     );
   }
 
-  const repositoryApiUrl = String(resolvedRemote.repositoryApiUrl).replace(/\/+$/, '');
-  const repositoryBrowserUrl = String(resolvedRemote.repositoryBrowserUrl).replace(/\/+$/, '');
+  const repositoryApiUrl = String(remote.repositoryApiUrl).replace(/\/+$/, '');
+  const repositoryBrowserUrl = String(remote.repositoryBrowserUrl).replace(/\/+$/, '');
 
   const api = (path) => `${repositoryApiUrl}${path}`;
 
@@ -57,24 +53,5 @@ function createRemoteRepositoryAPI(remote, urlPath = '') {
     resolveTag,
     branchesContaining,
     browseDocumentation,
-  };
-}
-
-
-function legacyGitHubRemoteToUrls(remote) {
-  if (!remote || !remote.host || !remote.repository) {
-    return remote;
-  }
-
-  const host = String(remote.host).replace(/^https?:\/\//, '').replace(/\/+$/, '');
-  const repository = String(remote.repository).replace(/^\/+/, '').replace(/\/+$/, '');
-  const apiBase = host === 'github.com'
-    ? 'https://api.github.com'
-    : `https://${host}/api/v3`;
-
-  return {
-    kind: 'github',
-    repositoryApiUrl: `${apiBase}/repos/${repository}`,
-    repositoryBrowserUrl: `https://${host}/${repository}`,
   };
 }
