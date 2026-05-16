73f69661dacec26912c86502c742b72ce740217c
Dominic Mayers <dominic.mayers@meditationstudies.org>
2026-05-16 04:18:18 -0400

    Reformat

--- scripts/last-of-readme/install/run-installation-per-se.cjs
+++ scripts/last-of-readme/install/run-installation-per-se.cjs
@@ -1,23 +1,11 @@
 #!/usr/bin/env node
 
-const {
-  remindAboutConvenienceHooks,
-} = require('./remind-not-owned-hooks.cjs');
-const {
-  installDocLinkFile,
-} = require('./install-doc-link-file.cjs');
-const {
-  installRemoteConfig,
-} = require('./install-remote-config.cjs');
-const {
-  installDocLinkConfig,
-} = require('./install-doc-link-config.cjs');
-const {
-  installNonInteractivePolicy,
-} = require('./install-non-interactive-policy.cjs');
-const {
-  installOwnedVersionHooks,
-} = require('./install-owned-version-hooks.cjs');
+const { remindAboutConvenienceHooks } = require('./remind-not-owned-hooks.cjs');
+const { installDocLinkFile } = require('./install-doc-link-file.cjs');
+const { installRemoteConfig } = require('./install-remote-config.cjs');
+const { installDocLinkConfig } = require('./install-doc-link-config.cjs');
+const { installNonInteractivePolicy } = require('./install-non-interactive-policy.cjs');
+const { installOwnedVersionHooks } = require('./install-owned-version-hooks.cjs');
 
 async function runInstallationPerSe(pipelineState = {}) {
   pipelineState = installDocLinkFile(pipelineState);
